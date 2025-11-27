96412fab490fcbcbcdbb7f4cc15b51d29b55d107
Flourence <158169448+FlourenceL@users.noreply.github.com>
2025-11-27 10:14:24 +0800

    App module config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EthModule } from './app/eth/eth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), EthModule],
   controllers: [AppController],
   providers: [AppService],
 })
